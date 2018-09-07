0586b1c0d8710b1336b9fa02f5bc059532cad171
e <bxczp@hotmail.com>
2018-09-08 07:54:57 +0800

    init

---
+++ src/main/java/com/bxczp/service/impl/DemoProviderServiceImpl.java
@@ -0,0 +1,11 @@
+package com.bxczp.service.impl;
+
+import com.bxczp.service.DemoProviderService;
+
+public class DemoProviderServiceImpl implements DemoProviderService{
+
+	public String sayHello(String name) {
+		return "����001"+name;
+	}
+
+}
